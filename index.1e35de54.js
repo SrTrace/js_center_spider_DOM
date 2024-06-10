eb641741b62d29ebbf13c561e656d43077366338
SrTrace <levashov04sergey@gmail.com>
2024-06-10 12:17:33 +0300

    make build

---
+++ index.1e35de54.js
@@ -0,0 +1,2 @@
+var e=document.querySelector(".wall"),t=document.querySelector(".spider");t.style.top=e.clientHeight/2-t.width/2+"px",t.style.left=e.clientWidth/2-t.width/2+"px";
+//# sourceMappingURL=index.1e35de54.js.map
